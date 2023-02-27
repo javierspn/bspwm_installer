c7ea91f0aaaca9bf1b73eaf65496a7988225f2dd
javierspn <javier.santiago@msn.com>
2023-02-27 17:09:02 +0100

    More simplification!

--- .config/bin/set-monitors.sh
+++ .config/bin/set-monitors.sh
@@ -1,29 +1,9 @@
-get_primary_monitor_name () {
-    local xmain_monitor_name
-    if ! check_primary_monitor; then
-        xmain_monitor_name=$(xrandr -q | grep " connected [primary]" | cut -d ' ' -f 1)
-        echo "$xmain_monitor_name"
-   fi
-}
-
 get_monitor_name () {
   local xmonitor_name
     if check_primary_monitor; then
         xmonitor_name=$(xrandr -q | grep " connected [^primary]" | cut -d ' ' -f 1)
         echo "$xmonitor_name"
     fi
-}
-
-get_monitor_resolution () {
-  local monitor_name="$1"
-
-  if check_primary_monitor; then
-    resolution=$(xrandr -q | grep "$monitor_name connected [^primary]" | cut -d ' ' -f 4 | cut -d "+" -f 1)
-  else
-    resolution=$(xrandr -q | grep "$monitor_name connected [^primary]" | cut -d ' ' -f 3 | cut -d "+" -f 1)
-  fi
-
-echo $resolution
 }
 
 json_config_dir="$HOME/Documents/dev/eye/bspwm_installer/wm_data/base_files"
